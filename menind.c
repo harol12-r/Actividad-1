30e195a689446d4d318df6c20bc4d4a18c9e0bd1
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-13 15:06:08 -0500

    tercer commit del segundo ejercicio

--- menind.c
+++ menind.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main() {
    char vocales[] = {'a', 'e', 'i', 'o', 'u'};
-   char *ptr;
+   char *ptr, *aux;
    ptr = &vocales[0];
+   aux = &vocales[4];
+   *aux = 'x';
    for (int i = 0; i < 5; i++)
    {
        printf("vocales[%d] %c ", i, vocales[i]);
