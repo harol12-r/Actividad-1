4083187ceea4ccfe05204f6eef054fc59ee9a4ec
Harol Rosero <harol.rosero@udla.edu.ec>
2026-05-13 14:34:41 -0500

    segundo Commit

--- mend.c
+++ mend.c
@@ -2,9 +2,12 @@
 
 int main (int argc, char *argv[]) {
 int vec[5]={2,4,6,8,10};
+int*ptr;
+ptr=vec[0];
 for (int i = 0; i < 5; i++)
 {
-    printf("&d", vec[i]);
+    printf("vector[%d] %d", i,vec[i]);
+    printf("/n%p",*(ptr++));
 }
     return 0;
 }
